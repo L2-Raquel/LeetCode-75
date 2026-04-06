f6f4ecd598551448c99106efdad2522b8ec7222a
Raquel <l2raquel.art@gmail.com>
2026-04-06 18:02:52 +0200

    Prueba3

---
+++ facil/src/GreatestCommonDivisorOfStrings_1071/GCDOS.java
@@ -0,0 +1,8 @@
+package GreatestCommonDivisorOfStrings_1071;
+
+public class GCDOS {
+    public static void main(String[] args) {
+
+        //Prueba3
+    }
+}
--- facil/src/MergeStringsAlternately_1768/java/org/msa/MSA.java
+++ facil/src/MergeStringsAlternately_1768/java/org/msa/MSA.java
@@ -2,7 +2,7 @@ package org.msa;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
+public class MSA {
     public static void main(String[] args) {
 //Prueba2
     }
